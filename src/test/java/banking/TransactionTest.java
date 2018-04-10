e02d95d3347e9d44c6452c67404c17fcd0767228
Shawn Broyles <sbroyles3@hawkmail.hccfl.edu>
2018-04-10 15:15:01 -0400

    Added sources and importing the non-deprecated Assert to the test cases

--- src/test/java/banking/TransactionTest.java
+++ src/test/java/banking/TransactionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
