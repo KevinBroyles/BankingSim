6627264617e3a3c47dc4a6999f0aa0c4be88108b
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-04-09 22:47:24 -0400

    Added SavingsAccountTest file
    This is a class automatically generated by the JUnit wizard.

---
+++ src/banking/SavingsAccountTest.java
@@ -0,0 +1,124 @@
+package banking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SavingsAccountTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testDeposit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWithdraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSavingsAccountCustomerDoubleString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSavingsAccountCustomerDoubleStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddInterestTransaction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDefaultInterestRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDefaultInterestRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTransfer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAccountDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAccountDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTransaction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAccountId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCustomerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAccountCreationDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
