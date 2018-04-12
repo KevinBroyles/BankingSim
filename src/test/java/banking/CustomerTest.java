6e03b17acb1c79e501ce3ad8dee041b4d2b0b26f
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-04-11 23:28:16 -0400

    Added at least some tests for the remaining methods.
    I now have at least some tests for all the methods that I think need to
    be tested. I'll try to think of additional things I want to test but
    I've got a good start.

--- src/test/java/banking/CustomerTest.java
+++ src/test/java/banking/CustomerTest.java
@@ -64,13 +64,47 @@ public class CustomerTest {
 	}
 
 	@Test
+	public void testAddSavingsAccountNullReturnValue() {
+		Customer testCust = FirstBank.getCustomer("12345");
+		SavingsAccount newAcc = testCust.addSavingsAccount(-500, "New Standard Savings Account");
+		assertNotNull("Account Not Create", newAcc);
+	}
+	
+	@Test
 	public void testAddSavingsAccount() {
-		fail("Not yet implemented");
+		Customer testCust = FirstBank.getCustomer("12345");
+		int numBefore = testCust.getCustomerAccounts().size();
+		testCust.addSavingsAccount(500, "New Standard Savings Account");
+		int numAfter = testCust.getCustomerAccounts().size();
+		assertFalse("Customer Account Not added", numBefore >= numAfter);
+	}
+
+	@Test(expected=Exception.class)
+	public void testAddSavingsAccountNegativeBalance() {
+		Customer testCust = FirstBank.getCustomer("12345");
+		int numBefore = testCust.getCustomerAccounts().size();
+		// a new account should not start overdrawn
+		testCust.addSavingsAccount(-500, "New Standard Savings Account");
+		int numAfter = testCust.getCustomerAccounts().size();
+		assertFalse("Customer Account Not added", numBefore >= numAfter);
+	}
+
+	@Test
+	public void testRemoveAccountZeroLenString() {
+		Customer testCust = FirstBank.getCustomer("12345");
+		int numBefore = testCust.getCustomerAccounts().size();
+		// Account ID should not be 0 length
+		testCust.removeAccount("");
+		int numAfter = testCust.getCustomerAccounts().size();
+		assertFalse("Customer Account Removed", numBefore > numAfter);
 	}
 
 	@Test
 	public void testRemoveAccount() {
-		fail("Not yet implemented");
+		Customer testCust = FirstBank.getCustomer("12345");
+		int numBefore = testCust.getCustomerAccounts().size();
+		testCust.removeAccount("");
+		int numAfter = testCust.getCustomerAccounts().size();
+		assertFalse("Customer Not Account Removed", numBefore <= numAfter);
 	}
-
 }
